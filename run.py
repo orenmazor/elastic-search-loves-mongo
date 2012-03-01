c9513c6103fb6190340612a5172f1597545d0373
Oren Mazor <oren@wildbit.com>
2012-03-01 17:18:51 -0500

    fixed some bugs, pylint complaints

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
-import daemon
 from oplog import Oplog
 import elasticsearch_api
-import pdb
 import datetime
 from json import loads
 from requests import put
@@ -14,7 +12,7 @@ def run():
 	activity_queue = []
 
 	started = datetime.datetime.now()
-	last_queue_purge = started()
+	last_queue_purge = started
 	for operation,data in op_emitter.watch_oplog():
 		if operation == "i":
 			es_record = elasticsearch_api.remap(data)
@@ -23,11 +21,13 @@ def run():
 			activity_queue.append(elasticsearch_api.delete(data))
 	
 		#would look better with a timer object
-		if (datetime.datetime.now() - last_queue_clearing).seconds >= config["queue_purge_frequency"]:
+		now = datetime.datetime.now()
+		if (now - last_queue_purge).seconds >= config["queue_purge_frequency"]:
 			message = "\n".join(activity_queue)
 			res = put(config["elasticsearch"]["connectionString"]+config["elasticsearch"]["index"]+"/_bulk",data=message)
 			print "ES replied " + str(res.status_code)
 			activity_queue = []
+			last_queue_purge = now
 
 if __name__ == "__main__":
 	run()
