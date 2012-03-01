226314fabd3269fc69a54b222f0fa6de159e1410
Oren Mazor <oren@wildbit.com>
2012-03-01 16:51:00 -0500

    remove my mapping. nobody cares about that

--- elasticsearch_api.py
+++ elasticsearch_api.py
@@ -24,25 +24,4 @@ def delete(documentid,bulk=True):
 
 #in most cases we may not want to actual insert exactly what is in the mongo, so parse this
 def remap(original_document):
-	entry = {}
-	entry["OID"] = original_document["_id"]
-	entry["MID"] = original_document["MID"]
-	entry["RE"] = original_document["RE"]
-	entry["DE"] = original_document["DE"]
-	entry["TA"] = original_document["TA"]
-	entry["UID"] = original_document["UID"]
-	entry["SID"] = original_document["SID"]
-	entry["TY"] = original_document["TY"]
-	entry["SE"] = original_document["SE"]
-	entry["SU"] = original_document["SU"]
-	entry["BT"] = original_document["BT"]
-	entry["BI"] = original_document["BI"]
-	entry["BounceDescription"] = original_document["BounceDescription"]
-	entry["RA"] = str(original_document["RA"])
-	if "AT" in original_document and not original_document["AT"] == None and not original_document["AT"] == []:
-		entry["AT"] = dumps(original_document["AT"])
-	#if email is sent, make sure it's purged
-	if entry["TY"] == 1:
-		entry["_ttl"] = "15d"
-
-	return entry
+	return original_document
