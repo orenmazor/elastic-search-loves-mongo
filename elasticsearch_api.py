bdfc4a16cc156f78a76f2ac8170207d5b1eba5a9
Oren Mazor <oren@wildbit.com>
2012-03-02 15:03:49 -0500

    make oplog and es more reusable for external tools (i.e. scrutineer)

--- elasticsearch_api.py
+++ elasticsearch_api.py
@@ -1,27 +1,72 @@
-from json import dumps
+from json import dumps,loads
 from hashlib import sha224
+from datetime import datetime
+from threading import Timer
+from requests import put,get
 
-#each document needs to have a totally unique id. if they're ever in conflict, you'll overwrite documents passively
-def generate_document_id(items):
-	return sha224("".join(items)).hexdigest()
+class ElasticSearch:
+	#for bulk api calls, we'll queue up actions in here
+	activity_queue = []
+	clear_queue = False
 
-def index(document,bulk=True):
-	if not bulk:
-		pass
+	def __init__(self):
+		self.started = datetime.now()
+		config = loads(open("config.json").read())
+		self.time_to_purge_queue = Timer(config["queue_purge_frequency"],None)
+		self.time_to_purge_queue.start()
 
-	index_command = {"index":{"_index":"","_type":"","_id":generate_document_id([document["MID"],document["_id"]])}}
-	message = dumps(index_command) + "\n" + dumps(document) + "\n"
+	#each document needs to have a totally unique id. if they're ever in conflict, you'll overwrite documents passively
+	def generate_document_id(self,items):
+		return sha224("".join(map(str,items))).hexdigest()
+
+	def index(self,document,index,doctype,bulk=True):
+		if bulk:
+			index_command = {"index":{"_index":index,"_type":doctype,"_id":self.generate_document_id([document["MID"],document["_id"]])}}
+			message = dumps(index_command) + "\n" + dumps(document) + "\n"
+			self.activity_queue.append(message)
+
+			#is it time to purge the queue?
+			if not self.time_to_purge_queue.isAlive():
+				self.purge_queue()
+				self.time_to_purge_queue.start()
+		else:
+			pass
+
+	def delete(self,documentid,index,doctype,bulk=True):
+		if bulk:
+			delete_command = {"delete":{"_index":index,"_type":doctype,"_id":""}}
+			self.activity_queue.append(dumps(delete_command))
+
+			#is it time to purge the queue?
+			if not self.time_to_purge_queue.isAlive():
+				self.purge_queue()
+				self.time_to_purge_queue.start()
+		else:
+			pass
+
+	def scroll_search(self,doctype,query):
+		req = get(self.config["elasticsearch"]["connectionString"]+self.config["elasticsearch"]["index"] + doctype+"/_search?search_type=scan&scroll=10m",data=dumps(query))
+
+		if not req.status_code == 200:
+			raise Exception(req.content)
+
+		res = loads(req.content)
+		total_hits = res['hits']['total']
+		print "got back " + str(total_hits) + " results"
+		scroll_id = res['_scroll_id']
+
+		new_total_hits = total_hits
+		while not new_total_hits == 0:
+			new_req = get(self.config["elasticsearch"]["connectionString"]+ '_search/scroll?scroll=10m',data=str(scroll_id))
+			if not new_req.status_code == 200:
+				raise Exception(new_req.content)
+
+			new_res = loads(new_req.content)
+			for esdoc in new_res['hits']['hits']:
+				yield esdoc
 
 
-	return message
-
-def delete(documentid,bulk=True):
-	if not bulk:
-		pass
-
-	delete_command = {"delete":{"_index":"","_type":"","_id":""}}
-	return dumps(delete_command)
-
-#in most cases we may not want to actual insert exactly what is in the mongo, so parse this
-def remap(original_document):
-	return original_document
+	def purge_queue(self):
+		message = "\n".join(self.activity_queue)
+		res = put(self.config["elasticsearch"]["connectionString"]+self.config["elasticsearch"]["index"]+"/_bulk",data=message)
+		print str(datetime.now()) + " - purged queue of " + len(self.activity_queue) + " items. ES responded with: " + str(res.status_code)
--- oplog.py
+++ oplog.py
@@ -2,79 +2,90 @@ import pymongo
 from pymongo.errors import AutoReconnect
 from json import loads
 import sys
+import hooks
 
 class Oplog:
 
-	def __init__(self):
-		self.last_record = None
+    def __init__(self):
+        self.last_record = None
 
-		try:
-			self.config = loads(open("config.json").read())
-		except:
-			print "failed to read config.json"
-			sys.exit()
+        try:
+            self.config = loads(open("config.json").read())
+        except:
+            print "failed to read config.json"
+            sys.exit()
 
-	def watch_oplog(self):
-		oplog_collection = None
-		actual_data = None
+    def start(self):
+        for op,namespace,data in self.watch_oplog(self):
+            if op == "i":
+                hooks.mongo_add_event(namespace,data)
+            elif op == "d":
+                hooks.mongo_delete_event(namespace,data)
+            
+    def get_oplog_database(self):
+        #open a connection to the oplog collection
+        try:
+            connection = pymongo.Connection(self.config["oplog"]["host"],self.config["oplog"]["port"])
+            db = connection[self.config["oplog"]["db"]]
+            oplog_collection = db[self.config["oplog"]["collection"]]
+            return oplog_collection
+        except:
+            print "failed to connect to oplog"
+            sys.exit()       
 
-		#open a connection to the oplog collection
-		try:
-			connection = pymongo.Connection(self.config["oplog"]["host"],self.config["oplog"]["port"])
-			db = connection[self.config["oplog"]["db"]]
-			oplog_collection = db[self.config["oplog"]["collection"]]
-		except:
-			print "failed to connect to oplog"
-			sys.exit()
+    def get_actual_database(self):
+        #open a connection to the db.
+        try:
+            connection = pymongo.Connection(self.config["data"]["host"],self.config["data"]["port"])
+            actual_data = connection[self.config["data"]["db"]]
+            return actual_data
+        except:
+            print "failed to connect to actual database"
+            sys.exit()
 
-		#open a connection to the db.
-		try:
-			connection = pymongo.Connection(self.config["data"]["host"],self.config["data"]["port"])
-			actual_data = connection[self.config["data"]["db"]]
-		except:
-			print "failed to connect to actual database"
-			sys.exit()
+    def watch_oplog(self):
+        oplog_collection = self.get_oplog_database()
+        actual_data = self.get_actual_database()
 
+        #run forevermore
+        while True:
+            try:
+                #oplogs are in a natural order, so we dont need to actually
+                #iterate to find the latest insertion. we can just grab the last entry.
+                #and maybe back up a little bit. you dont have to if you dont want to.
+                last_index = oplog_collection.count() - 100 
+                cursor = oplog_collection.find(skip=last_index,tailable=True)
 
-		#run forevermore
-		while True:
-			try:
-				#oplogs are in a natural order, so we dont need to actually
-				#iterate to find the latest insertion. we can just grab the last entry.
-				#and maybe back up a little bit. you dont have to if you dont want to.
-				last_index = oplog_collection.count() - 100 
-				cursor = oplog_collection.find(skip=last_index,tailable=True)
+                while cursor.alive:
+                    for op in cursor:
 
-				while cursor.alive:
-					for op in cursor:
+                        #parse the namespace of the op to figure out what collection its coming from
+                        try:
+                            op_collection_name = op['ns'].split('.')[1]
+                        except:
+                            continue
 
-						#parse the namespace of the op to figure out what collection its coming from
-						try:
-							op_collection_name = op['ns'].split('.')[1]
-						except:
-							continue
+                        #if its a collection we want to index, dump it
+                        if not op_collection_name in self.config["types_we_care_about"]:
+                            continue
 
-						#if its a collection we want to index, dump it
-						if not op_collection_name in self.config["types_we_care_about"]:
-							continue
+                        if op['op'] == 'i':
+                            yield ('i',op['o'],op_collection_name)
+                        elif op['op'] == 'u':
+                            #get the latest version of this document. luckily, we get the criteria
+                            criteria = op['o2']
+                            data_cursor = actual_data[op_collection_name]
+                            docs = data_cursor.find(criteria)
 
-						if op['op'] == 'i':
-							yield ('i',op['o'])
-						elif op['op'] == 'u':
-							#get the latest version of this document. luckily, we get the criteria
-							criteria = op['o2']
-							data_cursor = actual_data[op_collection_name]
-							docs = data_cursor.find(criteria)
+                            #its basically impossible for there to be more than one document here. 
+                            #but just in case
+                            for doc in docs:
+                                yield ('i',doc,op_collection_name)
 
-							#its basically impossible for there to be more than one document here. 
-							#but just in case
-							for doc in docs:
-								yield ('i',doc)
+                        elif op['op'] == 'd':
+                            yield ('d',op['o']['_id'],op_collection_name)
 
-						elif op['op'] == 'd':
-							yield ('d',op['o']['_id'])
-
-			except AutoReconnect:
-				#log this terrible fact
-				#continue
-				pass
+            except AutoReconnect:
+                #log this terrible fact
+                #continue
+                pass
